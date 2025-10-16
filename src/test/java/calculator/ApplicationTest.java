ba5a2f106b431ea6482078c1e7b490c57b0e3cc2
kimsz123456 <kimsz123456@gmail.com>
2025-10-16 19:50:08 +0900

    test: 예외처리 테스트 추가
    1. 커스텀 구분자 형식이 잘못된 경우
    2. 커스텀 구분자가 1글자가 아닌 경우
    3. 커스텀 구분자가 없는 경우
    4. 숫자가 아닌 값을 입력할 경우
    5. 입력 숫자가 정수 범위를 초과하는 경우
    6. 입력 숫자의 합계가 정수 범위를 초과하는 경우

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -19,8 +19,56 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_형식이_잘못된_경우_예외가_발생한다() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자가_1글자가_아닌_경우_예외가_발생한다() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;;\\n1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자가_없는_경우_예외가_발생한다() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//\\n1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자가_아닌_값_입력시_예외가_발생한다() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,a,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 입력_숫자가_정수_범위를_초과하면_예외가_발생한다() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("2147483648"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 합계가_정수_범위를_초과하면_예외가_발생한다() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("2147483647,1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
